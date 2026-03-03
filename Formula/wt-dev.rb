fc6bc6b4375cb90bd29e673f64f57b4d20749cdb
Yuki Arita <yuki@arita.email>
2026-03-04 08:11:52 +0900

    feat: add versioned release v0.1.0

--- Formula/wt-dev.rb
+++ Formula/wt-dev.rb
@@ -1,8 +1,10 @@
 class WtDev < Formula
   desc "Worktree x devcontainer parallel development environment"
   homepage "https://github.com/raben/wt-dev"
-  head "https://github.com/raben/wt-dev.git", branch: "main"
+  url "https://github.com/raben/wt-dev/archive/refs/tags/v0.1.0.tar.gz"
+  sha256 "be572a035528ac71b6c7753f431c8d8bdfae54e10038a898fbd8f34a4e310669"
   license "MIT"
+  head "https://github.com/raben/wt-dev.git", branch: "main"
 
   depends_on "jq"
 
