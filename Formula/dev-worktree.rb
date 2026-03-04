5d3eeb4589d3cc8ac6742964ce0807eaff0ee026
Yuki Arita <yuki@arita.email>
2026-03-04 14:00:02 +0900

    rename: wt-dev → dev-worktree

---
+++ Formula/dev-worktree.rb
@@ -0,0 +1,18 @@
+class DevWorktree < Formula
+  desc "Isolated parallel development environments with worktree + devcontainer"
+  homepage "https://github.com/raben/dev-worktree"
+  url "https://github.com/raben/dev-worktree/archive/refs/tags/v0.3.0.tar.gz"
+  sha256 "479405990536bd09278b4ef7b28a05f6ae1c79928e3ff0af3b72fcd6782d6b03"
+  license "MIT"
+  head "https://github.com/raben/dev-worktree.git", branch: "main"
+
+  depends_on "jq"
+
+  def install
+    bin.install Dir["bin/*"]
+  end
+
+  test do
+    system "#{bin}/dev", "--version"
+  end
+end
