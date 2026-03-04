f85489c99672dda26f6542e5fd53c2e782f7578f
Yuki Arita <yuki@arita.email>
2026-03-04 13:47:49 +0900

    feat: v0.2.0 - single 'dev' binary with subcommands

--- Formula/wt-dev.rb
+++ Formula/wt-dev.rb
@@ -1,8 +1,8 @@
 class WtDev < Formula
-  desc "Worktree x devcontainer parallel development environment"
+  desc "Isolated parallel development environments with worktree + devcontainer"
   homepage "https://github.com/raben/wt-dev"
-  url "https://github.com/raben/wt-dev/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "be572a035528ac71b6c7753f431c8d8bdfae54e10038a898fbd8f34a4e310669"
+  url "https://github.com/raben/wt-dev/archive/refs/tags/v0.2.0.tar.gz"
+  sha256 "3360f364c71c0a41c69acbcf84583ab7be7d2cb178e83dd939311ba242194f96"
   license "MIT"
   head "https://github.com/raben/wt-dev.git", branch: "main"
 
@@ -13,6 +13,6 @@ class WtDev < Formula
   end
 
   test do
-    system "#{bin}/wt-port-registry", "--help"
+    system "#{bin}/dev", "--version"
   end
 end
