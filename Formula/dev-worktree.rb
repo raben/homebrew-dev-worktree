7da611c018813b516a568ed5e6727af2bbb9bffc
Yuki Arita <yuki@arita.email>
2026-03-07 16:29:16 +0900

    bump to v0.9.6

--- Formula/dev-worktree.rb
+++ Formula/dev-worktree.rb
@@ -1,8 +1,8 @@
 class DevWorktree < Formula
   desc "Isolated parallel development environments with worktree + devcontainer"
   homepage "https://github.com/raben/dev-worktree"
-  url "https://github.com/raben/dev-worktree/archive/refs/tags/v0.9.5.tar.gz"
-  sha256 "ca395ca26a5ee763fa06d06ae8c2e3483147f2276e55da254741810ffe7465eb"
+  url "https://github.com/raben/dev-worktree/archive/refs/tags/v0.9.6.tar.gz"
+  sha256 "24891554e6d28a11f08dd84059cb71f334d5f961f1fab8aaee25226a64fb6082"
   license "MIT"
   head "https://github.com/raben/dev-worktree.git", branch: "main"
 
